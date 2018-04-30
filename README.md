7789248bfab914bd2b122fe3bf731ff85b776248
haag <sim.paris@wanadoo.fr>
2018-04-30 20:51:08 +0200

    fight_risk v1, bug pattern res

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Journey_Project
+# Journey Project
+
+L'idée est de partir d'une grande question existentielle : qu'elles sont mes chances de victoire quand je commence une bataille à Risk.
+Une fois que j'aurai codé ça, je me pencherai sur l'implémentation d'une map. et puis sur une mécanique de jeu (pas forcément fidèle au Risk). À terme j'aimerait avoir un backend assez bien designé pour pouvoir, d'ici quelques dizaines d'heures de travails, implémenter un frontend.
+L'agilité voudrait que j'iter dès le début avec ma partie frontend. Seulement c'est le backend qui m'intéresse dans un premier temps et je n'ai pas envie de perdre du temps sur le frontend en ce début de projet.
+
+`todo` bug : l'execution successive du script avec les mêmes paramètres donne le même pattern resultat O_o
+
+*Simon P.*
---
+++ fight_risk.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "sort"
+    "os"
+    "strconv"
+)
+
+
+func main() {
+
+	l_offense,_ := strconv.Atoi(os.Args[1])
+	l_defense,_ := strconv.Atoi(os.Args[2])
+	l_accuracy,_ := strconv.Atoi(os.Args[3])
+
+	l_res := 0
+	for c_accuracy := 0; c_accuracy < l_accuracy; c_accuracy++ {
+		l_res += fight(l_offense, l_defense)
+	}
+
+	fmt.Println(strconv.Itoa(l_res * 100 / l_accuracy) + "% to win !")
+}
+
+// fight like Risk
+func fight(p_offense_amount int, p_defense_amount int) int {
+	var l_offense_dice []int
+	var l_defense_dice []int
+
+	if p_offense_amount <= 1 {
+		fmt.Println("attack failed")
+		return 0	// attack failed
+	} else if p_offense_amount == 2 {
+		l_offense_dice = append(l_offense_dice, rand.Intn(6))
+	} else if p_offense_amount == 3 {
+		l_offense_dice = append(l_offense_dice, rand.Intn(6))
+		l_offense_dice = append(l_offense_dice, rand.Intn(6))
+	} else if p_offense_amount > 3 {
+		l_offense_dice = append(l_offense_dice, rand.Intn(6))
+		l_offense_dice = append(l_offense_dice, rand.Intn(6))
+		l_offense_dice = append(l_offense_dice, rand.Intn(6))
+	}
+
+	if p_defense_amount <= 0 {
+		fmt.Println("assault successed")
+		return 1	// assault successed
+	} else if p_defense_amount == 1 {
+		l_defense_dice = append(l_defense_dice, rand.Intn(6))
+	} else if p_defense_amount >= 2 {
+		l_defense_dice = append(l_defense_dice, rand.Intn(6))
+		l_defense_dice = append(l_defense_dice, rand.Intn(6))
+	}
+
+	sort.Ints(l_offense_dice)
+	sort.Ints(l_defense_dice)
+
+	l_offense_amount := p_offense_amount
+	l_defense_amount := p_defense_amount
+
+	for c_battle := 0; c_battle < min(len(l_offense_dice), len(l_defense_dice)); c_battle++ {
+		if l_offense_dice[c_battle] > l_defense_dice[c_battle] {
+			l_defense_amount--
+		} else {
+			l_offense_amount--
+		}
+	}
+	
+	return fight(l_offense_amount, l_defense_amount)
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
